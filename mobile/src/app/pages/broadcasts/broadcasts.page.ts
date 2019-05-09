00b1d805aef5aea4751d66879318a0eb8326ca2b
Aldi Rohman <aldirohman@live.com>
2019-05-09 09:02:40 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ mobile/src/app/pages/broadcasts/broadcasts.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-broadcasts',
+  templateUrl: './broadcasts.page.html',
+  styleUrls: ['./broadcasts.page.scss'],
+})
+export class BroadcastsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
