73e0fb39f8a45d193c9a7569fc563ea2c5b7076d
Aldi Rohman <aldirohman@live.com>
2019-05-10 13:41:57 +0700

    integrasi API broadcast

--- mobile/src/app/pages/broadcasts/broadcasts.page.ts
+++ mobile/src/app/pages/broadcasts/broadcasts.page.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { BroadcastService } from '../../services/broadcast.service';
+import { LoadingController } from '@ionic/angular';
+import { Broadcast } from '../../interfaces/broadcast';
 
 @Component({
   selector: 'app-broadcasts',
   templateUrl: './broadcasts.page.html',
-  styleUrls: ['./broadcasts.page.scss'],
+  styleUrls: ['./broadcasts.page.scss']
 })
 export class BroadcastsPage implements OnInit {
+  dataBroadcast: Broadcast[];
+  dataEmpty = false;
+  currentPage = 1;
+  maximumPages: number;
 
-  constructor() { }
+  constructor(
+    private broadcastService: BroadcastService,
+    public loadingCtrl: LoadingController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+    this.getNomorBroadcasts();
   }
 
+  // get data nomor penting
+  async getNomorBroadcasts() {
+    // check internet
+    if (!navigator.onLine) {
+      alert('Tidak ada jaringan internet');
+      return;
+    }
+
+    const loader = await this.loadingCtrl.create({
+      duration: 10000
+    });
+    loader.present();
+
+    this.dataEmpty = false;
+
+    this.broadcastService.getListBroadCasts().subscribe(
+      res => {
+        if (res['data']['items'].length) {
+          this.dataBroadcast = res['data']['items'];
+        } else {
+          this.dataEmpty = true;
+        }
+        // set count page
+        this.maximumPages = res['data']['_meta'].pageCount;
+        loader.dismiss();
+      },
+      err => {
+        loader.dismiss();
+      }
+    );
+  }
 }
