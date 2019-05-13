5baf52982c838d3754e9ba663c2746b4d6863124
aldi <aldirohman@live.com>
2019-05-13 14:27:27 +0700

    add UI list notifikasi & budget

--- mobile/src/app/pages/notifikasi/notifikasi.page.ts
+++ mobile/src/app/pages/notifikasi/notifikasi.page.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notifikasi',
   templateUrl: './notifikasi.page.html',
-  styleUrls: ['./notifikasi.page.scss'],
+  styleUrls: ['./notifikasi.page.scss']
 })
 export class NotifikasiPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goToDetail() {
+    console.log('masuk');
   }
-
 }
