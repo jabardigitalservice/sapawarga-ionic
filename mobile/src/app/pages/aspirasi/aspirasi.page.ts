ad0ab0a3e6b7e98712616d130249e108c81e96ec
Yoga Hanggara <yohang88@gmail.com>
2019-05-13 15:34:21 +0700

    aspirasi go back
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- mobile/src/app/pages/aspirasi/aspirasi.page.ts
+++ mobile/src/app/pages/aspirasi/aspirasi.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 
 import records from '../../../assets/data/aspirasi-list';
 
