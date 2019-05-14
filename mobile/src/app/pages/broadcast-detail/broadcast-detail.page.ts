ade2e4b28b4e5ac621653e081c6b17aa1464d356
aldi <aldirohman@live.com>
2019-05-14 10:01:48 +0700

    add function service & integrasi detail broadcast

--- mobile/src/app/pages/broadcast-detail/broadcast-detail.page.ts
+++ mobile/src/app/pages/broadcast-detail/broadcast-detail.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import {
+  NavController,
+  LoadingController,
+  ToastController
+} from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { Broadcast } from '../../interfaces/broadcast';
+import { BroadcastService } from '../../services/broadcast.service';
 
 @Component({
   selector: 'app-broadcast-detail',
@@ -7,9 +14,48 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./broadcast-detail.page.scss']
 })
 export class BroadcastDetailPage implements OnInit {
-  constructor(private navCtrl: NavController) {}
+  idBroadcast: number;
+  dataBroadcast: Broadcast;
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private broadcastService: BroadcastService,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // get id detail Broadcast
+    this.route.params.subscribe(params => {
+      this.idBroadcast = params['id'];
+    });
+
+    if (this.idBroadcast) {
+      this.getDetaiBroadcast();
+    }
+  }
+
+  // get data nomor penting
+  async getDetaiBroadcast() {
+    const loader = await this.loadingCtrl.create({
+      duration: 10000
+    });
+    loader.present();
+
+    this.broadcastService.getDetailBroadCast(this.idBroadcast).subscribe(
+      res => {
+        this.dataBroadcast = res['data'];
+        console.log(this.dataBroadcast);
+        loader.dismiss();
+      },
+      err => {
+        loader.dismiss();
+        this.showToast(err.data.message);
+        // jika data not found
+        this.navCtrl.back();
+      }
+    );
+  }
 
   // Called when view is left
   ionViewWillLeave() {
@@ -19,5 +65,13 @@ export class BroadcastDetailPage implements OnInit {
 
   backButton() {
     this.navCtrl.navigateForward('/tabs/broadcasts');
+  }
+
+  async showToast(msg: string) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 }
