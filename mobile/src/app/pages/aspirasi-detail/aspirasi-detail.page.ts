c95a9732f81ddf4878b30e1c5ed1697fcc81f968
aldidoanta <aldidkurnia@gmail.com>
2019-05-23 14:51:07 +0700

    [Aspirasi] Modify detail aspirasi page

--- mobile/src/app/pages/aspirasi-detail/aspirasi-detail.page.ts
+++ mobile/src/app/pages/aspirasi-detail/aspirasi-detail.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
-
-import records from '../../../assets/data/aspirasi-list';
+import {
+  LoadingController,
+  ToastController,
+  NavController
+} from '@ionic/angular';
+import { AspirasiService } from '../../services/aspirasi.service';
+import { Aspirasi } from '../../interfaces/aspirasi';
 
 @Component({
   selector: 'app-aspirasi-detail',
@@ -10,10 +15,14 @@ import records from '../../../assets/data/aspirasi-list';
 })
 export class AspirasiDetailPage implements OnInit {
   id: number;
-  record = {};
-
+  dataAspirasi: Aspirasi;
   constructor(
       private route: ActivatedRoute,
+      private aspirasiService: AspirasiService,
+      private loadingCtrl: LoadingController,
+      private toastCtrl: ToastController,
+      private navCtrl: NavController,
+      private router: Router
   ) { }
 
   ngOnInit() {
@@ -21,10 +30,35 @@ export class AspirasiDetailPage implements OnInit {
       this.id = params['id'];
     });
 
-    this.getData(this.id);
+    this.getDetailAspirasi();
   }
 
-  getData(id) {
-    this.record = records[id - 1];
+  // get data nomor penting
+  async getDetailAspirasi() {
+    const loader = await this.loadingCtrl.create({
+      duration: 10000
+    });
+    loader.present();
+
+    this.aspirasiService.getDetailAspirasi(this.id).subscribe(
+      res => {
+        this.dataAspirasi = res['data'];
+        loader.dismiss();
+      },
+      err => {
+        loader.dismiss();
+        this.showToast(err.data.message);
+        // jika data not found
+        this.navCtrl.back();
+      }
+    );
+  }
+
+  async showToast(msg: string) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 }
