72e9036b1ac81feb6887a5f7ae47d7c3d552810b
aldidoanta <aldidkurnia@gmail.com>
2019-06-11 14:28:57 +0700

    [Onboarding] Add uncommitted changes [ci skip]

--- mobile/src/app/onboarding/onboarding.page.ts
+++ mobile/src/app/onboarding/onboarding.page.ts
@@ -1,53 +1,59 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-onboarding",
-  templateUrl: "./onboarding.page.html",
-  styleUrls: ["./onboarding.page.scss"]
+  selector: 'app-onboarding',
+  templateUrl: './onboarding.page.html',
+  styleUrls: ['./onboarding.page.scss']
 })
 export class OnboardingPage implements OnInit {
   slides = [
     {
-      title: "Selamat datang di Sapa Warga",
-      description: "Menghubungkan Anda dengan Pemerintah Provinsi Jawa Barat",
-      titleColor: "#000000"
+      title: 'Selamat datang di Sapa Warga',
+      description: 'Menghubungkan Anda dengan Pemerintah Provinsi Jawa Barat',
+      titleColor: '#000000'
     },
     {
-      title: "Aspirasi",
+      title: 'Aspirasi',
       description:
-        "Sampaikan saran, keluhan, dan ide Anda melalui fitur polling, survei, dan pelaporan kepada pemerintah daerah.",
-      titleColor: "#3ca96b"
+        'Sampaikan saran, keluhan, dan ide Anda melalui fitur polling, survei, dan pelaporan kepada pemerintah daerah.',
+      titleColor: '#3ca96b'
     },
     {
-      title: "Informasi",
+      title: 'Informasi',
       description:
-        "Dapatkan broadcast informasi penting dan terbaru dari pemerintah.",
-      titleColor: "#fecb38"
+        'Dapatkan broadcast informasi penting dan terbaru dari pemerintah.',
+      titleColor: '#fecb38'
     },
     {
-      title: "Pelayanan Publik",
+      title: 'Pelayanan Publik',
       description:
-        "Nikmati pelayanan publik dan proses administrasi yang lebih mudah dan efisien.",
-      titleColor: "#00b7f0"
+        'Nikmati pelayanan publik dan proses administrasi yang lebih mudah dan efisien.',
+      titleColor: '#00b7f0'
     }
   ];
 
-  slideCaptionStyle = ["slide-caption bottom", "slide-caption top"];
+  slideCaptionStyle = ['slide-caption bottom', 'slide-caption top'];
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
   getSlideBackgroundStyle(index) {
-    const verticalPos = index % 2 ? "center" : "bottom";
+    const verticalPos = index % 2 ? 'center' : 'bottom';
 
     return {
-      width: "100%",
-      height: "100%",
-      position: "relative",
+      width: '100%',
+      height: '100%',
+      position: 'relative',
       background: `url("../../assets/img/onboarding/onboarding${index}.png") no-repeat`,
       backgroundPosition: `center ${verticalPos}`,
-      backgroundSize: "cover"
+      backgroundSize: 'cover'
     };
+  }
+
+  goToLogin() {
+    localStorage.setItem('has-onboarding', 'true');
+    this.router.navigate(['/login']);
   }
 }
