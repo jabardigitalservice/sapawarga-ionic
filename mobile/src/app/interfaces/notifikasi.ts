3ed834b30a0e971dcbff9ccb39de0c94c855d9b0
aldidoanta <aldidkurnia@gmail.com>
2019-06-18 16:25:08 +0700

    [Notification] Handle initial state [ci skip]

--- mobile/src/app/interfaces/notifikasi.ts
+++ mobile/src/app/interfaces/notifikasi.ts
@@ -7,4 +7,5 @@ export interface Notifikasi {
   target: string;
   title: string;
   updated_at: number;
+  read: boolean;
 }
--- mobile/src/app/pages/notifikasi/notifikasi.page.ts
+++ mobile/src/app/pages/notifikasi/notifikasi.page.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Notifikasi } from '../../interfaces/notifikasi';
@@ -12,19 +13,57 @@ import records from '../../../assets/data/notifikasi';
 export class NotifikasiPage implements OnInit {
   dataNotifikasi: Notifikasi[];
   records: [];
+  interval: any;
 
   constructor(
     private notifikasiService: NotifikasiService,
+    private route: ActivatedRoute,
     public navCtrl: NavController
   ) {
     this.records = records;
   }
 
   ngOnInit() {
+    this.route.queryParamMap.subscribe(params => {
+      // const newNotifikasi = params['params'];
+      console.log(params);
+
+      // Check if empty object
+      // if (Object.keys(newNotifikasi).length !== 0) {
+      // }
+    });
+
+    this.updateNotifikasiBadge();
+  }
+
+  ionViewDidEnter() {
     this.dataNotifikasi = this.notifikasiService.getLocalNotifikasi() || [];
+
+    // listen and render component
+    this.interval = setInterval(() => {
+      this.dataNotifikasi = this.notifikasiService.getLocalNotifikasi() || [];
+    }, 3000);
+    this.updateNotifikasiBadge();
+  }
+
+  ionViewWillLeave() {
+    window.clearInterval(this.interval);
   }
 
   goToDetail(target) {
     this.navCtrl.navigateForward(target);
+  }
+
+  updateNotifikasiBadge() {
+    const readCount = this.dataNotifikasi.reduce(
+      (count, notif) => (notif.read === true ? ++count : count),
+      0
+    );
+
+    if (readCount === this.dataNotifikasi.length) {
+      this.notifikasiService.setNotifikasiBadge(false);
+    } else {
+      this.notifikasiService.setNotifikasiBadge(true);
+    }
   }
 }
