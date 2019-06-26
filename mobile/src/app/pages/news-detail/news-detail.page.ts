566d023827315a329129dd7805cfc0fb525f1fec
aldi <aldirohman@live.com>
2019-06-26 17:15:00 +0700

    styling detail news, list news other

--- mobile/src/app/pages/news-detail/news-detail.page.ts
+++ mobile/src/app/pages/news-detail/news-detail.page.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-news-detail',
   templateUrl: './news-detail.page.html',
-  styleUrls: ['./news-detail.page.scss'],
+  styleUrls: ['./news-detail.page.scss']
 })
 export class NewsDetailPage implements OnInit {
+  dummyHeadlines = [
+    {
+      id: 1,
+      title: 'Pose Ridwan Kamil Mejeng di Depan Mobil klasik VW',
+      image: 'assets/img/aspirasi/aspirasi2.jpg',
+      source: 'detik.com'
+    },
+    {
+      id: 2,
+      title: 'Pose Ridwan Kamil Mejeng di Depan Mobil klasik VW',
+      image: 'assets/img/aspirasi/aspirasi1.jpg',
+      source: 'detik.com'
+    },
+    {
+      id: 3,
+      title: 'Pose Ridwan Kamil Mejeng di Depan Mobil klasik VW',
+      image: 'assets/img/aspirasi/aspirasi3.jpg',
+      source: 'detik.com'
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
