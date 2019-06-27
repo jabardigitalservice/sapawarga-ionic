8e3cf6c6186df9bd477fac87ddfcce658ef83bf3
aldi <aldirohman@live.com>
2019-06-27 10:27:40 +0700

    integrasi service news

--- mobile/src/app/services/news.service.ts
+++ mobile/src/app/services/news.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { News } from '../interfaces/news';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getListNews(): Observable<News[]> {
+    return this.http
+      .get<News[]>(`${environment.API_MOCK}/news`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError(error.error);
+  }
 }
