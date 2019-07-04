2d2f65df8135d1c4f08e16858ee79716943935dd
aldi <aldirohman@live.com>
2019-07-04 16:52:19 +0700

    add local URL to mock server

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL: 'http://35.247.135.93.xip.io/api/v1',
-  API_MOCK: 'http://35.247.135.93:3000/api/v1',
+  API_MOCK: 'http://10.59.3.164:3000/api/v1',
   API_STORAGE: 'http://35.247.135.93/api/storage',
   VERSION_APP: '0.0.1'
 };
