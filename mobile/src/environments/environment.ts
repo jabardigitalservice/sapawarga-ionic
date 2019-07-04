7b532f271ac9139a53fcd81fc8786d2af16df892
aldi <aldirohman@live.com>
2019-07-04 14:33:24 +0700

    change URL mock server

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   API_URL: 'http://35.247.135.93.xip.io/api/v1',
   // API_URL: 'http://10.59.3.157/api/v1',
   API_STORAGE: 'http://35.247.135.93/api/storage',
-  API_MOCK: 'http://35.247.135.93:3000/api/v1',
+  API_MOCK: 'http://10.59.3.164:3000/api/v1',
   VERSION_APP: '0.0.1'
 };
