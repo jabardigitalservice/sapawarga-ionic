807d7495e59049843d26ef6bda25376fd8d30b7b
aldi <aldirohman@live.com>
2019-07-09 14:09:44 +0700

    add skeleton loading

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://35.247.135.93.xip.io/api/v1',
+  API_URL: '35.247.135.93:81/v1',
   API_MOCK: 'http://10.59.3.164:3000/api/v1',
   API_STORAGE: 'http://35.247.135.93/api/storage',
   VERSION_APP: '0.0.1'
