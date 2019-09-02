d70f3655032323b316079da4cdb32f209127ff5a
a.rohman <aldirohman@live.com>
2019-09-02 11:43:51 +0700

    add API production

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://35.247.135.93:81/v1',
-  API_MOCK: 'http://10.59.3.164:3000/api/v1',
-  API_STORAGE: 'http://35.247.135.93:81/storage'
+  API_URL: 'https://sapawarga.jabarprov.go.id/api/v1',
+  API_STORAGE: 'https://sapawarga.jabarprov.go.id/storage',
+  API_MOCK: 'http://10.59.3.164:3000/api/v1'
 };
