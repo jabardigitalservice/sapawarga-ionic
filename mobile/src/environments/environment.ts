564de019098310f5a9d9868424aa9c4acf942b4d
a.rohman <aldirohman@live.com>
2019-09-18 16:46:14 +0700

    change URL staging

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_URL: 'http://35.247.135.93:81/v1',
+  API_URL: 'http://52.74.74.33:81/v1',
   API_MOCK: 'http://10.59.3.164:3000/api/v1',
-  API_STORAGE: 'http://35.247.135.93:81/storage'
+  API_STORAGE: 'http://52.74.74.33:81/storage'
 };
