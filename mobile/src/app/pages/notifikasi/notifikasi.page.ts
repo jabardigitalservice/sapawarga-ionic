b94281f8dc341b82ab3531ee6823d3413347419f
a.rohman <aldirohman@live.com>
2019-10-14 08:43:24 +0700

    fix navigation menu notification

--- mobile/src/app/pages/notifikasi/notifikasi.page.ts
+++ mobile/src/app/pages/notifikasi/notifikasi.page.ts
@@ -59,20 +59,17 @@ export class NotifikasiPage implements OnInit {
     window.clearInterval(this.interval);
   }
 
-  goToDetail(index: number, meta: any) {
-    if (meta.target === 'polling') {
-      const navigationParams = [`/${meta.target}`];
-      if (meta.id) {
-        navigationParams.push(meta.id);
-      }
-      this.router.navigate(navigationParams);
-    } else if (meta.target === 'survey' || meta.target === 'url') {
-      if (meta.target === 'survey' && !meta.url) {
-        this.router.navigate([`/${meta.target}`]);
-      } else {
-        this.launchWeb(meta);
-      }
+  goToDetail(index: number, meta: any, target: string) {
+    if (target === 'url') {
+      this.inAppBrowser.create(meta.url, '_system'); // call webview in app
+    } else if (target === 'notifikasi' && meta.target === 'survey') {
+      this.util.launchweb(meta.url); // call webview external
+    } else if (target === 'notifikasi' && meta.target === 'url') {
+      this.inAppBrowser.create(meta.url, '_system'); // call yotube app
+    } else {
+      this.router.navigate([`/${meta.target}`, meta.id]);
     }
+
     this.dataNotifikasi[index].read = true;
     this.notifikasiService.saveLocalNotifikasi(
       JSON.stringify(this.dataNotifikasi)
@@ -88,7 +85,7 @@ export class NotifikasiPage implements OnInit {
   }
 
   getImageURL(targetName: string) {
-    const prefix = '../../../assets/icon';
+    const prefix = 'assets/icon';
     switch (targetName) {
       case 'survey':
         return `${prefix}/SW-SURVEY.png`;
@@ -98,6 +95,9 @@ export class NotifikasiPage implements OnInit {
         break;
       case 'url':
         return `${prefix}/SW-NOPENTING.png`;
+        break;
+      case 'saber-hoax':
+        return `${prefix}/saber_hoax.png`;
         break;
       default:
         return `${prefix}/SW-ASPIRASI.png`;
