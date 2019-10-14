9ae1a9c48c0e450351d0e2b33a50a354889f4176
a.rohman <aldirohman@live.com>
2019-10-14 08:54:56 +0700

    change API staging

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   API_URL: 'http://52.74.74.33:81/v1',
-  API_MOCK: 'http://10.59.3.164:3000/api/v1',
-  API_STORAGE: 'http://52.74.74.33:81/storage'
+  API_MOCK: 'https://sapawarga-staging.jabarprov.go.id',
+  API_STORAGE: 'https://sapawarga-staging.jabarprov.go.id/storage'
 };
