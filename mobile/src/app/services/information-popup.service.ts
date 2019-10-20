26054988f8f40c74a8dd07427af76d283f9e26f4
aldi <aldirohman@live.com>
2019-10-20 18:14:59 +0700

    create logic information popup

--- mobile/src/app/services/information-popup.service.ts
+++ mobile/src/app/services/information-popup.service.ts
@@ -13,26 +13,7 @@ import { Constants } from '../helpers/constants';
   providedIn: 'root'
 })
 export class InformationPopupService {
-  dataPopup = {
-    id: 1,
-    title: 'West Java Investment Summit 2019',
-    image_path: 'media/EG5lFOhUEAAXA79',
-    image_path_url:
-      'https://pbs.twimg.com/media/EG5lFOhUEAAXA79?format=jpg&name=small',
-    type: 'external',
-    link_url: 'https://twitter.com/humasjabar/status/1183994898569023490',
-    internal_category: null,
-    internal_entity_id: null,
-    internal_entity_name: null,
-    status: 10,
-    status_label: 'Aktif',
-    created_at: 1570177071,
-    updated_at: 1570177071,
-    created_by: 1,
-    updated_by: 1,
-    start_date: '2019-10-16',
-    end_date: '2019-10-19'
-  };
+  dataPopup: any;
 
   constructor(
     private http: HttpClient,
@@ -42,46 +23,51 @@ export class InformationPopupService {
   ) {}
 
   checkInformationPopup() {
-    // this.getInformationPopup().subscribe(respons => {
-    //   console.log(respons.data.items[0]);
-    // this.dataPopup = respons.data.items[0];
-    // });
+    this.getInformationPopup().subscribe(respons => {
+      // console.log(respons.data.items[0]);
+      this.dataPopup = respons.data.items[0];
 
-    // this.showModal();
+      const lastDatePopup = this.getStoragePopup();
+      const currentDate = formatDate(new Date(), 'yyyy/MM/dd', 'en');
+      const endDate = formatDate(
+        new Date(this.dataPopup.end_date),
+        'yyyy/MM/dd',
+        'en'
+      );
+      const getCompareDate = this.compareDate(
+        currentDate,
+        endDate,
+        lastDatePopup
+      );
+      // this.setStoragePopup(currentDate);
+      console.log(getCompareDate);
+      console.log(lastDatePopup);
+      this.showModal();
 
-    const getDataPopup = localStorage.getItem(
-      this.constants.localStorage.informationPopup
-    );
-
-    const currentDate = formatDate(new Date(), 'yyyy/MM/dd', 'en');
-    const endDate = formatDate(new Date('2019-10-17'), 'yyyy/MM/dd', 'en');
-    const getCompareDate = this.compareDate(currentDate, endDate);
-    console.log(getCompareDate);
-    // console.log(now);
+      if (getCompareDate > 0) {
+        this.showModal();
+      }
+    });
   }
 
-  compareDate(date1: any, date2: any): number {
-    // With Date object we can compare dates them using the >, <, <= or >=.
-    // The ==, !=, ===, and !== operators require to use date.getTime(),
-    // so we need to create a new instance of Date with 'new Date()'
-    let d1 = new Date(date1);
-    let d2 = new Date(date2);
-
-    // console.log(d1.getTime());
-    // console.log(d2.getTime());
-
-    // if (date1 < date2) {
-    // }
-
-    // Check if the dates are equal
-    let same = d1.getTime() === d2.getTime();
-    if (same) return 0;
-
-    // Check if the first is greater than second
-    if (d1 > d2) return 1;
+  compareDate(currentDate: any, endDate: any, lastDatePopup: any): number {
+    // compare dates, create a new instance of Date with 'new Date()'
+    const current = new Date(currentDate);
+    const end = new Date(endDate);
+    const last = new Date(lastDatePopup);
 
     // Check if the first is less than second
-    if (d1 < d2) return -1;
+    if (current <= end) {
+      // Check if the dates are not equal
+      if (current.getTime() !== last.getTime()) {
+        this.setStoragePopup(currentDate);
+        return 1;
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
   }
 
   private getInformationPopup(): Observable<any> {
@@ -90,10 +76,21 @@ export class InformationPopupService {
       .pipe(catchError(this.util.handleError));
   }
 
+  setStoragePopup(value: string) {
+    localStorage.setItem(this.constants.localStorage.informationPopup, value);
+  }
+
+  getStoragePopup() {
+    return localStorage.getItem(this.constants.localStorage.informationPopup)
+      ? localStorage.getItem(this.constants.localStorage.informationPopup)
+      : '';
+  }
+
   async showModal() {
     const modal = await this.modalController.create({
       component: InformationPopupComponent,
-      cssClass: 'popup-information'
+      cssClass: 'popup-information',
+      backdropDismiss: false
     });
     return await modal.present();
   }
